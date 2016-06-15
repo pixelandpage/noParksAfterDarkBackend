51a494bde8d7b5d1646d656bb1582f6d242e40c3
Macey Baker <maceybaker@Maceys-MacBook-Pro-6.local>
2016-06-15 13:49:44 +0100

    Configured dotenv

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var hereAppID = process.env.HERE_APP_ID;
+var hereAppCode = process.env.HERE_APP_CODE;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
