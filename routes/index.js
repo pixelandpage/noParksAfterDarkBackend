3f313a7e784a3d5561573e35a2a1e9289b79b16b
Macey Baker <maceybaker@Maceys-MacBook-Pro-5.local>
2016-06-15 15:38:45 +0100

    Added API call

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+var url = "";
 var hereAppID = process.env.HERE_APP_ID;
 var hereAppCode = process.env.HERE_APP_CODE;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/route', function(req, res, next) {
+  request.get(url, function(error, response, body) {
+    console.log("REQ: START");
+    console.log(body);
+    console.log("REQ: END");
+  });
+  // res.render('index', { title: 'Express' });
+});
+
+router.post('/route', function(req, res, next){
 });
 
 module.exports = router;
