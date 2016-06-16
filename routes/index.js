9b98b614c312325f3bca076efc3a1b966f6839ee
pixelandpage <mags@pixelandpage.co.uk>
2016-06-16 13:19:45 +0100

    changed body to bodyObject in res.send()

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var hereAppID = process.env.HERE_APP_ID;
 var hereAppCode = process.env.HERE_APP_CODE;
 
 var url = "http://geocoder.cit.api.here.com/6.2/geocode.json";
+var searchArgument = "200%20S%20Mathilda%20Sunnyvale%20CA";
+var searchText = "?searchtext="+searchArgument;
 var appId = "&app_id="+hereAppID;
 var appCode = "&app_code="+hereAppCode;
 var gen = "&gen=8";
@@ -21,7 +23,7 @@ router.get('/route', function(req, res, next) {
     console.log("REQ: START");
     var bodyObject = JSON.parse(body);
     console.log(bodyObject.Response.View[0].Result[0].Location);
-    res.send(body); //stringify = opposite of parse
+    res.send(bodyObject); //stringify = opposite of parse
     console.log("REQ: END");
   });
   // res.render('index', { title: 'Express' });
