7d1b7c0ecbf1471ed1b2c64fe52e5872d30da744
Macey Baker <maceybaker@Maceys-MacBook-Pro-5.local>
2016-06-16 16:39:12 +0100

    Added routing api call

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ var request = require('request');
 var router = express.Router();
 var hereAppID = process.env.HERE_APP_ID;
 var hereAppCode = process.env.HERE_APP_CODE;
+var routeUrl = 'https://route.cit.api.here.com/routing/7.2/calculateroute.json';
 
-var url = "http://geocoder.cit.api.here.com/6.2/geocode.json";
+var locationUrl = "http://geocoder.cit.api.here.com/6.2/geocode.json";
 var searchText = "?searchtext=";
-var appId = "&app_id="+hereAppID;
+var locationAppId = "&app_id="+hereAppID;
+var routeAppId = "?app_id="+hereAppID;
 var appCode = "&app_code="+hereAppCode;
 var gen = "&gen=8";
+var start = '&waypoint0=geo!52.5,13.4';
+var end = '&waypoint1=geo!52.5,13.45';
+var mode = '&mode=fastest;car;traffic:disabled';
 
 // var util.format()
 router.use(function(req, res, next) {
@@ -18,7 +23,7 @@ router.use(function(req, res, next) {
 });
 
 router.get('/location/api', function(req, res, next) {
-  request.get(url + searchText + req.query.searchtext + appId + appCode + gen, function(error, response, body) { //massive cognitive load
+  request.get(locationUrl + searchText + req.query.searchtext + locationAppId + appCode + gen, function(error, response, body) { //massive cognitive load
     console.log("REQ: START");
     var bodyObject = JSON.parse(body);
     console.log(bodyObject.Response.View[0].Result[0].Location);
@@ -27,6 +32,19 @@ router.get('/location/api', function(req, res, next) {
   });
   // res.render('index', { title: 'Express' });
 });
+
+router.get('/route/api', function(req, res, next) {
+  request.get(routeUrl + routeAppId + appCode + start + end + mode, function(error, response, body) { //massive cognitive load
+    console.log("REQ: START");
+    // var bodyObject = JSON.parse(body);
+    // console.log(bodyObject.Response.View[0].Result[0].Location);
+    console.log(body);
+    res.send(body); //stringify = opposite of parse
+    console.log("REQ: END");
+  });
+  // res.render('index', { title: 'Express' });
+});
+
 
 // router.post('/route', function(req, res, next){
 // });
