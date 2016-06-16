dd932fca72c0c679b09b1e8479fa5e950c1030f1
pixelandpage <mags@pixelandpage.co.uk>
2016-06-16 16:06:55 +0100

    passing params into api query string

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,7 @@ var hereAppID = process.env.HERE_APP_ID;
 var hereAppCode = process.env.HERE_APP_CODE;
 
 var url = "http://geocoder.cit.api.here.com/6.2/geocode.json";
-var searchArgument = "200%20S%20Mathilda%20Sunnyvale%20CA";
-var searchText = "?searchtext="+searchArgument;
+var searchText = "?searchtext=";
 var appId = "&app_id="+hereAppID;
 var appCode = "&app_code="+hereAppCode;
 var gen = "&gen=8";
@@ -18,8 +17,8 @@ router.use(function(req, res, next) {
   next();
 });
 
-router.get('/route', function(req, res, next) {
-  request.get(url + searchText + appId + appCode + gen, function(error, response, body) { //massive cognitive load
+router.get('/location/api', function(req, res, next) {
+  request.get(url + searchText + req.query.searchtext + appId + appCode + gen, function(error, response, body) { //massive cognitive load
     console.log("REQ: START");
     var bodyObject = JSON.parse(body);
     console.log(bodyObject.Response.View[0].Result[0].Location);
