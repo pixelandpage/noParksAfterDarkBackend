0b49993960d6848c93ffc2882de6d9fb662d3e9a
Macey Baker <maceybaker@Maceys-MacBook-Pro-2.local>
2016-06-17 15:13:20 +0100

    Updated routing

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 var hereAppID = process.env.HERE_APP_ID;
 var hereAppCode = process.env.HERE_APP_CODE;
 var routeUrl = 'https://route.cit.api.here.com/routing/7.2/calculateroute.json';
-
 var locationUrl = "http://geocoder.cit.api.here.com/6.2/geocode.json";
 var searchText = "?searchtext=";
 var locationAppId = "&app_id="+hereAppID;
@@ -12,13 +11,9 @@ var routeAppId = "?app_id="+hereAppID;
 var appCode = "&app_code="+hereAppCode;
 var gen = "&gen=8";
 var start = '&waypoint0=geo!' + '51.51747,-0.08266';
-var end = '&waypoint1=geo!'+ '52.5,13.45';
-// var end = '&waypoint1=geo!52.5,13.45';
-// var start = '&waypoint0=geo!';
-// var end = '&waypoint1=geo!';
-var mode = '&mode=fastest;car;traffic:disabled';
+var end = '&waypoint1=geo!';
+var mode = '&mode=fastest;pedestrian;';
 
-// var util.format()
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -27,35 +22,33 @@ router.use(function(req, res, next) {
 
 
   router.get('/location/api', function(req, res, next) {
-    request.get(locationUrl + searchText + req.query.searchtext + locationAppId + appCode + gen, function(error, response, body) { //massive cognitive load
+    request.get(locationUrl + searchText + req.query.searchtext + locationAppId + appCode + gen, function(error, response, body) {
       console.log("REQ: START");
       var bodyObject = JSON.parse(body);
-      var lat = bodyObject.Response.View[0].Result[0].Location.NavigationPosition[0].Latitude;
-      var long = bodyObject.Response.View[0].Result[0].Location.NavigationPosition[0].Longitude;
-      var address = bodyObject.Response.View[0].Result[0].Location.Address.Label;
-        function getWaypointData(){
-          var waypointData = lat.toString() + ',' + long.toString();
-          return waypointData;
-        }
-        var end = '&waypoint1=geo!'+ getWaypointData();
-      console.log(getWaypointData());
-      res.send(bodyObject); //stringify = opposite of parse
+      res.send(bodyObject);
       console.log("REQ: END");
     });
   });
 
 router.get('/route/api', function(req, res, next) {
-  request.get(routeUrl + routeAppId + appCode + start + end + mode, function(error, response, body) { //massive cognitive load
+  request.get(locationUrl + searchText + req.query.searchtext + locationAppId + appCode + gen, function(error, response, body) {
     console.log("REQ: START");
-    console.log(body);
-    console.log(req.params);
-    res.send(body); //stringify = opposite of parse
+    var bodyObject = JSON.parse(body);
+    var lat = bodyObject.Response.View[0].Result[0].Location.NavigationPosition[0].Latitude;
+    var long = bodyObject.Response.View[0].Result[0].Location.NavigationPosition[0].Longitude;
+      function getWaypointData(){
+        var waypointData = lat.toString() + ',' + long.toString();
+        return waypointData;
+      }
+      var end = '&waypoint1=geo!'+ getWaypointData();
+    console.log("REQ: END");
+  request.get(routeUrl + routeAppId + appCode + start + end + mode, function(error, response, body) {
+    console.log("REQ: START");
+    res.send(body);
     console.log("REQ: END");
   });
 });
+});
 
-
-// router.post('/route', function(req, res, next){
-// });
 
 module.exports = router;
