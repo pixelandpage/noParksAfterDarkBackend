60d54d084e4b70e705cc9b1a8fd0302ae2b10a57
Ruta <ruta@baltiejus.com>
2016-06-20 12:35:18 +0100

    fixed failing tests

--- test/test.js
+++ test/test.js
@@ -15,31 +15,27 @@ describe('server response', function () {
 // });
 
 
-  it('returns 200', function(done) {
-    request.get('')
-      .expect(200)
-      .end(function(err, res){
+  it('heroku app is online with status 200', function(done) {
+    request.get('').expect(200, function(err, res){
+      done(err);
+    });
+  });
+
+  it('location/api route is accessible with status 200', function(done) {
+      request.get('/location/api').expect(200, function(err, res){
         done(err);
       });
   });
 
-  it('returns 200', function(done) {
-    request.get('/location/api')
-      .expect(200)
-      .end(function(err, res){
-        done(err);
-      });
-  });
-
-  it('can use route/api',function(done){
+  it('route/api route is accessible with status 200',function(done){
     request.get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
+      .expect(200)
       .end(function(err, res){
-        expect(res.status).to.equal(200);
         done(err);
       });
     });
 
-  it('returns JSON when using location/api',function(done){
+  it('location/api route returns JSON',function(done){
     request.get('/location/api/?searchtext=e15aq')
       .expect('Content-Type', /json/)
       .end(function(err, res){
@@ -48,29 +44,21 @@ describe('server response', function () {
       });
   });
 
-  it('returns JSON when using route/api',function(done){
+  it('route/api route returns JSON',function(done){
     request.get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
       .expect('Content-Type', /json/)
       .end(function(err, res){
-        done(err);
-      });
-  });
-  //
-  it('should return the correct long and lat for commercial street when using location/api',function(done){
-    request.get('/location/api/?searchtext=50%20commercial%20street%20london')
-    .expect('Content-Type', /json/)
-      .expect(function(res) {
-        expect(res).to.contain('51.5173562');
-      })
-      .end(function(err, res){
+        expect(res.status).to.equal(200);
         done(err);
       });
   });
 
-  it('returns JSON when using route/api',function(done){
-    request.get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
-      .expect('Content-Type', /json/)
+  it('should return the correct long and lat for commercial street when using location/api',function(done){
+    request.get('/location/api/?searchtext=50%20commercial%20street%20london')
+    .expect('Content-Type', /json/)
       .end(function(err, res){
+        expect(res.body.Response.View[0].Result[0].Location.NavigationPosition[0].Latitude).to.equal(51.5173562);
+        expect(res.body.Response.View[0].Result[0].Location.NavigationPosition[0].Longitude).to.equal(-0.0735515);
         done(err);
       });
   });
