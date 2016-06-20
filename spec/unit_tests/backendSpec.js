4844e947c0ab253cdb651f5718870a400440eef8
Macey Baker <maceybaker@Maceys-MacBook-Pro-2.local>
2016-06-20 10:15:29 +0100

    Got tests functioning

--- spec/unit_tests/backendSpec.js
+++
@@ -1,7 +0,0 @@
-describe('BackendApi', function() {
-  var Index = require('../../routes/index.js');
-
-  it("should be able to go to routes", function() {
-    
-  });
-});
--- test/test.js
+++ test/test.js
@@ -1,49 +1,77 @@
-var request = require("supertest");
 var express = require('express');
+var request = require('supertest')('https://no-parks-after-dark-backend.herokuapp.com');
+var expect = require('Chai').expect;
+var server = require('../lib/server.js');
+var chai = require('chai');
 var app = express();
 
-describe('BackendApi', function(){
-  it('can use location/api',function(){
-    request(app)
-      .get('/location/api')
-      .end(function(res){
+describe('server response', function () {
+//   before(function () {
+//   server.listen(3000);
+// });
+//
+// after(function () {
+//   server.close();
+// });
+
+
+  it('returns 200', function(done) {
+    request.get('')
+      .expect(200)
+      .end(function(err, res){
+        done(err);
+      });
+  });
+
+  it('returns 200', function(done) {
+    request.get('/location/api')
+      .expect(200)
+      .end(function(err, res){
+        done(err);
+      });
+  });
+
+  it('can use route/api',function(done){
+    request.get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
+      .end(function(err, res){
         expect(res.status).to.equal(200);
+        done(err);
+      });
     });
-  });
 
-  it('can use route/api',function(){
-    request(app)
-      .get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
-      .end(function(res){
+  it('returns JSON when using location/api',function(done){
+    request.get('/location/api/?searchtext=e15aq')
+      .expect('Content-Type', /json/)
+      .end(function(err, res){
         expect(res.status).to.equal(200);
-    });
+        done(err);
+      });
   });
 
-  it('returns JSON when using location/api',function(){
-    request(app)
-      .get('/location/api')
-      .expect('Content-Type', /json/);
+  it('returns JSON when using route/api',function(done){
+    request.get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
+      .expect('Content-Type', /json/)
+      .end(function(err, res){
+        done(err);
+      });
   });
-
-  it('returns JSON when using route/api',function(){
-    request(app)
-      .get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
-      .expect('Content-Type', /json/);
-  });
-
-  it('should return the correct long and lat for commercial street when using location/api',function(){
-    request(app)
-      .get('/location/api/?searchtext=50%20commercial%20street%20london')
+  //
+  it('should return the correct long and lat for commercial street when using location/api',function(done){
+    request.get('/location/api/?searchtext=50%20commercial%20street%20london')
+    .expect('Content-Type', /json/)
       .expect(function(res) {
-        res.body.Response.View[0].Result[0].Location.NavigationPostion[0].Latitude = '51.5173562';
-        res.body = 'Logitude:-0.0773git s515';
+        expect(res).to.contain('51.5173562');
       })
-      .expect('Content-Type', /json/);
+      .end(function(err, res){
+        done(err);
+      });
   });
 
-  // it('returns JSON when using route/api',function(){
-  //   request(app)
-  //     .get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
-  //     .expect('Content-Type', /json/);
-  // });
+  it('returns JSON when using route/api',function(done){
+    request.get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
+      .expect('Content-Type', /json/)
+      .end(function(err, res){
+        done(err);
+      });
+  });
 });
