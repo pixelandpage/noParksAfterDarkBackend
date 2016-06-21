bfdf5fdf55c5c6daad591e00a40c4735dacb5d52
Ruta <ruta@baltiejus.com>
2016-06-21 11:11:19 +0100

    tests check if route avoids parks

--- test/test.js
+++ test/test.js
@@ -63,93 +63,31 @@ describe('Backend Api', function() {
       assert.equal(200, res.statusCode);
       expect(res).to.be.json;
       expect(res.body.Response.View[0].Result[0].Location.NavigationPosition[0].Latitude).to.equal(51.5173562);
-
+      expect(res.body.Response.View[0].Result[0].Location.NavigationPosition[0].Longitude).to.equal(-0.0735515);
       done();
     });
   });
+
+  it('location/api should return error if incorrect params', function(done) {
+    chai.request('http://localhost:3000')
+      .get('/location/api/searchtext=50%20commercial%20street%20london')
+    .end(function (err, res) {
+      assert.equal(404, res.statusCode);
+      done();
+    });
+  });
+
+  it('route/api should avoid parks and tunnels when using public transport', function(done) {
+    chai.request('http://localhost:3000')
+      .get('/route/api/?endtext=e1%206lt&starttext=ha5%202ez&type=publicTransportTimeTable&nightmode=park:-1,tunnel:-1')
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        assert.equal(200, res.statusCode);
+        expect(res).to.be.json;
+        expect(res.body.response.route[0].mode.transportModes).to.include('publicTransportTimeTable');
+        expect(res.body.response.route[0].mode.feature[0].value).to.equal('park');
+        expect(res.body.response.route[0].mode.feature[1].value).to.equal('tunnel');
+        done();
+      });
+  });
 });
-
-
-
-
-
-//   it('should return the correct long and lat for commercial street when using location/api',function(done){
-//     request.get('/location/api/?searchtext=50%20commercial%20street%20london')
-//     .expect('Content-Type', /json/)
-//       .end(function(err, res){
-//         console.log(res);
-//         expect(res.body.Response.View[0].Result[0].Location.NavigationPosition[0].Latitude).to.equal(51.5173562);
-//         expect(res.body.Response.View[0].Result[0].Location.NavigationPosition[0].Longitude).to.equal(-0.0735515);
-//         done(err);
-//       });
-//   });
-
-//
-// describe('server', function() {
-// before(function() {
-//   server.listen(3000);
-// });
-//
-// after(function() {
-//   server.close();
-// });
-//
-//
-//
-//   it('app is online with status 200', function(done) {
-//     request.get('/').expect(200, function(err, res){
-//       done(err);
-//     });
-//   });
-//
-//   it('location/api route is accessible with status 200', function(done) {
-//       request.get('https://localhost:3000/location/api').expect(200, function(err, res){
-//         done(err);
-//       });
-//   });
-//
-//   it('route/api route is accessible with status 200',function(done){
-//     request.get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
-//       .expect(200)
-//       .end(function(err, res){
-//         done(err);
-//       });
-//     });
-//
-//   it('location/api route returns JSON',function(done){
-//     request.get('/location/api/?searchtext=e15aq')
-//       .expect('Content-Type', /json/)
-//       .end(function(err, res){
-//         expect(res.status).to.equal(200);
-//         done(err);
-//       });
-//   });
-//
-//   it('route/api route returns JSON',function(done){
-//     request.get('/route/api/?starttext=e1%205aq&endtext=e1%206lt')
-//       .expect('Content-Type', /json/)
-//       .end(function(err, res){
-//         expect(res.status).to.equal(200);
-//         done(err);
-//       });
-//   });
-//
-//   it('route/api should return error if incorrect params',function(done){
-//     request.get('/route/api/?london,paris')
-//       .expect('Content-Type', /json/)
-//       .end(function(err, res){
-//         expect(res.status).to.equal(503);
-//         done(err);
-//       });
-//   });
-//
-//   it('location/api should return error if incorrect params',function(done){
-//     request.get('/locations/api')
-//       .end(function(err, res){
-//         console.log(res);
-//         expect(res.status).to.equal(404);
-//         done(err);
-//       });
-//   });
-//
-// });
